a53219aa1508f8d8821e83fbed04eef28b8cff8f
jeddy2023 <jkenosuh@gmail.com>
2024-12-11 08:57:40 +0100

    deployment issues

--- manage.py
+++ manage.py
@@ -3,12 +3,10 @@
 import os
 import sys
 
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 def main():
     """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,6 +16,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
 
 if __name__ == '__main__':
     main()
